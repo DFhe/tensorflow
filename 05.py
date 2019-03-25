36a73fdf12c5917feda715d39325a5be0c7f5578
fei <1153779329@qq.com>
2019-03-25 14:34:07 +0800

    'init01'

--- 05.py
+++ 05.py
@@ -1,1 +1,47 @@
-#手写数字识别
+# 非线性回归测试
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_data = np.linspace(-0.5, 0.5, 200)[:, np.newaxis]
+noise = np.random.normal(0, 0.02, x_data.shape)
+y_data = np.square(x_data) + noise
+
+x = tf.placeholder(tf.float32, [None, 1])
+y = tf.placeholder(tf.float32, [None, 1])
+
+# 定义神经网络中间层
+Weights_L1 = tf.Variable(tf.random_normal([1, 10]))
+biases_L1 = tf.Variable(tf.zeros([1, 10]))
+wx_plus_L1 = tf.matmul(x, Weights_L1) + biases_L1
+L1 = tf.nn.tanh(wx_plus_L1)
+
+# 定义输出层
+Weights_L2 = tf.Variable(tf.random_normal([10, 1]))
+biases_L2 = tf.Variable(tf.zeros([1, 1]))
+wx_plus_L2 = tf.matmul(L1, Weights_L2) + biases_L2
+
+
+pred = tf.nn.tanh(wx_plus_L2)
+
+#二次代价函数
+loss = tf.reduce_mean(tf.square(y-pred))
+#使用梯度下降法
+train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for _ in range(2000):
+        sess.run(train,feed_dict={
+            x:x_data,
+            y:y_data
+        })
+
+    pred_value = sess.run(pred,feed_dict={
+        x:x_data
+    })
+    plt.figure()
+    plt.scatter(x_data,y_data)
+    plt.plot(x_data,pred_value,'r-',lw=5)
+    plt.show()
---
+++ 06.py
@@ -0,0 +1,55 @@
+# 手写数字识别
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+# 载入数据集
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# 每个批次的大小
+batch_size = 50
+# 计算一共有多少个批次
+n_batch = mnist.train.num_examples // batch_size
+
+# 定义两个placeholder
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+
+
+# 创建一个简单的神经网络
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+predition = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# 二次代价函数
+loss = tf.reduce_mean(tf.square(y - predition))
+
+# 使用梯度下降法
+train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+init = tf.global_variables_initializer()
+
+# 求准确率的方法
+# tf.argmax(y,1)判断最大概率的标签（位置
+correct_predtion = tf.equal(tf.argmax(y, 1), tf.argmax(predition, 1))
+
+# 求准确率
+# tf.cast类型转化
+accuracy = tf.reduce_mean(tf.cast(correct_predtion, tf.float32))
+
+# 进行训练
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(21):  # 整体循环21次训练
+        for batch in range(n_batch):  # 整体执行一次图片循环
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            sess.run(train_step, feed_dict={
+                x: batch_xs,
+                y: batch_ys
+            })
+
+        acc = sess.run(accuracy, feed_dict={
+            x: mnist.test.images,
+            y: mnist.test.labels
+        })
+        print("Iter " + str(epoch) + " ,Test Accuracy:" + str(acc))
